60b2d5330c71ce496c49f6b4fd4390f62e21ce37
Sharthak1705 <sharthakjain1@gmail.com>
2025-01-19 17:49:56 +0530

    Update Code

--- chai/urls.py
+++ chai/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.chai_list, name='chai_list'),
     path('create/', views.chai_create,name= 'chai_create'),
     path('<int:chai_id>/edit/', views.chai_edit,name= 'chai_edit'),
-    path('<int:chai_id>/delete', views.chai_remove,name='chai_remove'),
+    path('<int:chai_id>/delete/', views.chai_remove,name='chai_remove'),
+    path('register/', views.register, name= 'register'),
 
 ] 
--- chai/views.py
+++ chai/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
 from .models import Chai
-from .forms import ChaiForm
+from .forms import ChaiForm, UserRegisterFrom
 from django.shortcuts import get_object_or_404, redirect
-# Create your views here.
+from django.contrib.auth.decorators import login_required 
+from django.contrib.auth import login
 
 def index(request):
     return render(request, 'index.html')
 
 def chai_list(request):
     chais = Chai.objects.all().order_by('-created_at')
-    return render(request,'chai_list.html', {'Chai':
+    return render(request,'chai_list.html', {'chais':
     chais})
 
+@login_required
 def chai_create(request):
     if request.method == "POST":
        form = ChaiForm(request.POST, request.FILES)
@@ -25,7 +27,7 @@ def chai_create(request):
         form = ChaiForm()
     return render(request,'chai_form.html', {'form': form} )
 
-   
+@login_required   
 def chai_edit(request, chai_id):
     chai = get_object_or_404(Chai, pk =chai_id, user =request.user)
     if request.method == 'POST':
@@ -40,7 +42,7 @@ def chai_edit(request, chai_id):
       form = ChaiForm(instance = chai)
     return render(request, 'chai_form.html', {'form':form})
 
-
+@login_required
 def chai_remove(request, chai_id):
     chai =  get_object_or_404(Chai, pk =chai_id, user = request.user)
     if request.method == 'POST':
@@ -48,5 +50,16 @@ def chai_remove(request, chai_id):
         return redirect('chai_list')
     return render(request, 'chai_confirm.html', {'chai': chai})
 
-
+def register(request):
+    if request.method=='POST':
+      form = UserRegisterFrom(request.POST)
+      if form.is_valid():
+          user = form.save(commit=False)
+          user.set_password(form.cleaned_data['password1'])
+          user.save()
+          login(request,user)
+          return redirect('chai_list')
+    else:
+        form = UserRegisterFrom()
+    return render(request, 'registration/register.html', {'form': form})
 
